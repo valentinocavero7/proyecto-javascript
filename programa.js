f9822757bf4e31ad5a1f2087e86a12d241a623c1
Mario <111661567+valentinocavero7@users.noreply.github.com>
2023-07-24 16:59:18 -0500

    agregar arrow function de multiply

--- programa.js
+++ programa.js
@@ -3,3 +3,6 @@ console.log("hi world");
 function multiply(a, b) {
  console.log("El resultado de la multiplicación es: " + a*b);
 }
+
+//arrow function de la anterior function
+const multiplicación = (a, b) =>console.log("El resultado es: " + a*b);
