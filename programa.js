b28445676c9eaedada309783e362cffc7c89522f
valentinocavero7 <111661567+valentinocavero7@users.noreply.github.com>
2023-07-24 21:06:24 -0500

    confirmar entendido el push

--- programa.js
+++ programa.js
@@ -9,3 +9,7 @@ const multiplicación = (a, b) =>console.log("El resultado es: " + a*b);
 
 const array_int = [3, 5, 7, 8];
 array_int.map(x => console.log(Math.pow(x, 2)));
+
+// estoy agrenado esta linea desde el github
+consolo.log("Creo que con este ya entendí el push"),
+ // la idea es un pull, ya que entendí el push
